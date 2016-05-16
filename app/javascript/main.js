3b86a53c9a1d9cb6db7c761bb20662c5e5710f41
anamariasosa <anamariasosam@gmail.com>
2016-05-15 23:04:14 -0500

    [add] ajax songs

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,3 +1,31 @@
+function showSongs(song) {
+	var source   =	$('#songs-template').html();
+	var template = Handlebars.compile(source);
+	
+	$.ajax({
+		url: 'https://api.spotify.com/v1/search',
+		data: {
+			q: song,
+			type: 'track'
+		}
+	})
+	.done(function(response) {
+		console.log("success");
+		$('#songs_container').html(template(response));
+		$('#inputSong').blur().focus();
+		setTimeout(function () {
+			$('.song').removeClass('hidden')
+			$('.song_image').addClass('zoomIn');
+			$('.song_item-title').addClass('zoomIn');
+			$('.song_item-text').addClass('zoomIn');
+			$('.song_audio').addClass('zoomIn');
+		}, 1000);
+	})
+
+}
 jQuery(document).ready(function($) {
-	$('#inputSong').on('submit', )
+	$('.form-search').on('submit', function(e) {
+		e.preventDefault();
+		showSongs($("#inputSong").val());
+	});
 });
